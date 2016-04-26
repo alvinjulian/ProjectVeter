2fded85cc3aa7c24a42989efd8b3a8be64ce7e73
Vallenteer <kristiantokelvin@yahoo.co.id>
2016-04-26 13:55:59 +0700

    kasir bisa masuk data.. :9

--- ProjectVeter/frm_Kasir.vb
+++ ProjectVeter/frm_Kasir.vb
@@ -19,12 +19,12 @@
     End Sub
 
     Private Sub cmbBarcode_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbBarcode.SelectedIndexChanged
-        Dim index As Integer = cmbBarcode.SelectedIndex
+        Dim index As Integer = cmbBarcode.SelectedIndex + 1 'array mulai dari 0, tp kita ada isi dari 1
 
         For value As Integer = 0 To _data.barcode.Length
             If cmbBarcode.Text = _data.barcode(index) Then
                 txtNama.Text = _data.nama(index).ToString.Trim
-                txtHarga.Text = _data.nama(index).ToString.Trim
+                txtHarga.Text = _data.harga_array(index).ToString.Trim
             End If
         Next
 
@@ -32,13 +32,19 @@
 
     Private Sub frm_Kasir_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
-        If _data.barcode.Count < 1 Then
+        If _data.barcode(1) = 0 Then
             MessageBox.Show("Tidak ada data barang! Silahkan masukan data barang di Form Gudang terlebih dahulu", "Data Kosong!", MessageBoxButtons.OK)
+
+
         Else
             'cmbBarcode.MaxDropDownItems = _data.barcode.Length
+            'Lbl1.Text = _data.barcode.Count.ToString
             cmbBarcode.Items.Clear()
-            For value As Integer = 0 To _data.barcode.Length
-                cmbBarcode.Items.Add(_data.barcode(value).ToString.Trim)
+            For Each value As Integer In _data.barcode
+                If value <> 0 Then
+                    cmbBarcode.Items.Add((value).ToString)
+                End If
+                'cmbBarcode.Items.Add(_data.barcode(value).ToString.Trim)
             Next
         End If
 
